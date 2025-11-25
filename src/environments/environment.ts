36248a9d90cedbd7f4ec61d3e1f2071f07b215dd
Matheus <matheus.cabral@evosystems.com>
2025-11-25 01:59:39 -0300

    padronização rota api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //baseAPIUrl: 'http://localhost:5272/api',
-  baseAPIUrl: 'https://api-bitfolio.sa-east-1.elasticbeanstalk.com/api'
+  baseAPIUrl: 'https://api.bitfolio.com.br/api'
 };
 
 /*
