25fa3b3d1b70791d7fb985b3040c20d509400938
Matheus <matheus.cabral@evosystems.com>
2025-12-05 06:19:23 -0300

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseAPIUrl: 'http://localhost:5272/api',
-  //baseAPIUrl: 'https://api.bitfolio.com.br/api'
+  //baseAPIUrl: 'http://localhost:5272/api',
+  baseAPIUrl: 'https://api.bitfolio.com.br/api'
 };
 
